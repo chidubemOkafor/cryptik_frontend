b3fb3559a653d4a04b429601e720dbc79d165784
Robinhood <okaforchidubem7@gmail.com>
2023-05-30 11:50:27 +0100

    added abi
    i added the abi

--- src/components/body/Main.jsx
+++ src/components/body/Main.jsx
@@ -5,12 +5,12 @@ import SelectToken from "./SelectToken";
 // import tokenImage from ".././images/images.png"
 import Web3 from "web3"
 
-
 const Main = () => {
     // toggle 
     const [openSettings, setOpenSettings] = useState(false);
     const [selToken, setSelToken] = useState(false);
     const openToken = () =>  setSelToken(!selToken)
+    const [tokenAddress, setTokenAddress] = useState(null)
     
     // state hooks
     const [priceA, setPriceA] = useState("")
@@ -18,20 +18,23 @@ const Main = () => {
     const changeValA=(event)=> {setPriceA(event.target.value)}
     const changeValB=(event) => {setPriceB(event.target.value)}
     const [balanceA, setBalanceA] = useState("")
+    const [ethBalance, setEthBalance] = useState('')
     const [balanceB, setBalanceB] = useState("")
 
     // for connecting to wallet
     const [accounts, setAccounts] = useState([])
-    const [coin, setCoin] =useState("ETH")
+    const [coin1, setCoin1] =useState("ETH")
+    const [coin2, setCoin2] = useState("WETH")
+ 
     const [web3, setWeb3] = useState(null)
-    const [provider, setProvider] = useState(null)
+  
     
     const isConnected = Boolean(accounts[0])
     const web3Testnet = new Web3("https://goerli.infura.io/v3/4eed25b0bc4342529e3e61363f2d8d1a") 
 
     // functions
     //for setting the token
-    const setToken = (name) =>  name
+
 
     const toggleSettings = () =>  setOpenSettings(!openSettings)
 
@@ -48,7 +51,11 @@ const Main = () => {
             //convert balance to eth
             const balanceInEth = parseFloat(web3Testnet.utils.fromWei(balance, "ether"))
             setBalanceA(balanceInEth.toFixed(6))
-    
+            setEthBalance(balanceInEth.toFixed(6))
+            
+
+             // Get token balance
+          
         }
         catch(err) {
           console.log(err)
@@ -62,7 +69,7 @@ const Main = () => {
         <div className="flex justify-center bg-gradient-to-r from-black via-gray-900 to-green-500 h-auto">
         <div className=" bg-green-400 border border-gray-400 my-20 w-[25em] rounded-md py-1 mb-40">
         <div className="flex justify-between h-auto p-5">
-        {selToken ? <SelectToken setToken={setToken}  className="justify-center flex"/> : <></>}
+      {selToken ? <SelectToken ethBalance={ethBalance} accounts={accounts} setBalanceA={setBalanceA} setTokenAddress={setTokenAddress} setCoin1={setCoin1} className="justify-center flex"/> : <></>}
 
         <div className="font-bold">
             <span>
@@ -91,7 +98,7 @@ const Main = () => {
             <div className="p-2 hover:border-2 border border-green-300 bg-green-300 rounded-md h-20">
                     <div className="flex justify-between items-center px-2">
                     <input onChange={changeValA} value={priceA} className="outline-none bg-green-300 h-10 text-3xl w-[8.2em] appearance-none" type="number" min="0" max="100000" style={{ appearance: "textfield", MozAppearance: "textfield" }}/>
-                <div className="bg-green-400 p-2 rounded-full flex gap-2 font-bold cursor-pointer" onClick={openToken}><div className="bg-green-600 h-7 w-7 rounded-full "/>{coin}</div>
+                <div className="bg-green-400 p-2 rounded-full flex gap-2 font-bold cursor-pointer" onClick={openToken}><div className="bg-green-600 h-7 w-7 rounded-full "/>{coin1}</div>
                     </div>
                     <div className="flex justify-between px-2"><div className="font-bold">{priceB * 2262}</div>
                 <div>Balance: {balanceA}</div>
@@ -102,7 +109,7 @@ const Main = () => {
             <div className="p-2 hover:border-2 border border-green-300 border-2 border-black bg-green-300 rounded-md h-20">
                     <div className="flex justify-between items-center px-2">
                     <input onChange={changeValB} value={priceB} className="outline-none bg-green-300 h-10 text-3xl w-[8.2em] appearance-none" type="number" min="0" max="100000" style={{ "-moz-appearance": "textfield", "-webkit-appearance": "textfield" }}/>
-                    <div className="bg-green-400 p-2 rounded-full flex gap-2 font-bold cursor-pointer"><div className="bg-green-600 h-7 w-7 rounded-full"/>ETH</div>
+                    <div className="bg-green-400 p-2 rounded-full flex gap-2 font-bold cursor-pointer" onClick={openToken}><div className="bg-green-600 h-7 w-7 rounded-full"/>{coin2}</div>
                     </div>
                     <div className="flex justify-between px-2"><div className="font-bold">{priceB * 2262}</div>
                 <div>Balance: {balanceB}</div>
